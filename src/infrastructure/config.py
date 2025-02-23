59ec3a88f63be7e8a6a25b807a733e5cb7096bff
Nikolay Koren <nikolai.koren2004@gmail.com>
2025-02-23 23:53:23 +0600

    chore: settings for api

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -8,6 +8,7 @@ class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
     echo: bool = False
@@ -24,7 +25,18 @@ class DatabaseConfig(BaseModel):
     }
 
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    finance: str = "/finance"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
 BASE_DIR = Path(__file__).parent.parent
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -35,6 +47,7 @@ class Settings(BaseSettings):
     )
     run: RunConfig = RunConfig()
     db: DatabaseConfig
+    api: ApiPrefix = ApiPrefix()
 
 
 settings = Settings()
